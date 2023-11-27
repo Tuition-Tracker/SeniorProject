c515f5d8013f2eed468d0b11e55c272d450a26bb
maiagervasone <maiagervasone@gmail.com>
2023-11-27 14:52:39 -0500

    Added Arizona schools

--- app/src/main/java/com/example/googlemapspractice/UniversityData.java
+++ app/src/main/java/com/example/googlemapspractice/UniversityData.java
@@ -47,6 +47,35 @@ public class UniversityData {
         universities.add(new University("Alaska", "University of Alaska Anchorage", 7508, 21635));
         universities.add(new University("Alaska", "University of Alaska Fairbanks", 7896, 22032));
         universities.add(new University("Alaska", "University of Alaska Southeast", 6960, 21096));
+        universities.add(new University("Arizona", "American InterContinental University", 12422, 12422));
+        universities.add(new University("Arizona", "Arizona Christian University", 31650, 31650));
+        universities.add(new University("Arizona", "Arizona College of Nursing - Phoenix", 24000, 24000));
+        universities.add(new University("Arizona", "Arizona College of Nursing - Tempe", 24000, 24000));
+        universities.add(new University("Arizona", "Arizona College of Nursing - Tucson", 22826, 22826));
+        universities.add(new University("Arizona", "Arizona State University Campus Immersion", 11618, 30592));
+        universities.add(new University("Arizona", "Arizona State University Digital Immersion", 10978, 13248));
+        universities.add(new University("Arizona", "Aspen University", 5355, 5355));
+        universities.add(new University("Arizona", "Brookline College - Phoenix", 89404, 89404));
+        universities.add(new University("Arizona", "Brookline College - Tempe", 30150, 30150));
+        universities.add(new University("Arizona", "Brookline College - Tucson", 31150, 31150));
+        universities.add(new University("Arizona", "Bryan University", 12184, 12184));
+        universities.add(new University("Arizona", "Chamberlain University - Arizona", 20142, 20142));
+        universities.add(new University("Arizona", "DeVry University - Arizona", 17488, 17488));
+        universities.add(new University("Arizona", "Dine College", 1410, 1410));
+        universities.add(new University("Arizona", "Embry-Riddle Aeronautical University - Prescott", 40464, 40464));
+        universities.add(new University("Arizona", "Grand Canyon University", 17800, 17800));
+        universities.add(new University("Arizona", "Indian Bible College", 7420, 7420));
+        universities.add(new University("Arizona", "International Baptist College and Seminary", 13200, 13200));
+        universities.add(new University("Arizona", "National Paralegal College", 7995, 7995));
+        universities.add(new University("Arizona", "Northern Arizona University", 12274, 27536));
+        universities.add(new University("Arizona", "Ottawa University - Phoenix", 12641, 12641));
+        universities.add(new University("Arizona", "Ottawa University - Surprise", 33000, 33000));
+        universities.add(new University("Arizona", "Pima Medical Institute - Tucson", 24475, 24475));
+        universities.add(new University("Arizona", "Prescott College", 35450, 35450));
+        universities.add(new University("Arizona", "Sessions College for Professional Design", 12440, 12440));
+        universities.add(new University("Arizona", "University of Advancing Technology", 18794, 18794));
+        universities.add(new University("Arizona", "University of Arizona", 13275, 39577));
+        universities.add(new University("Arizona", "University of Phoenix - Arizona", 9552, 9552));
     }
 }
 
